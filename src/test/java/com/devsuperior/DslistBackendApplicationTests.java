1d5462da0d283951251e037237a06aeb89412847
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 <paulovitorcarneiro@hotmail.com>
2023-11-20 23:24:55 -0300

    Game seed

---
+++ src/test/java/com/devsuperior/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
